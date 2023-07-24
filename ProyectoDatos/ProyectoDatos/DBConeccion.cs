15d0540dbbac96a06e41ac73fe0e2bda07257385
KarlaCornejo <kcornejop@unsa.edu.pe>
2023-07-24 00:55:24 -0500

    Desarrollo de Laboratorio 8
    Inicio de Sesion y Cookies

---
+++ ProyectoDatos/ProyectoDatos/DBConeccion.cs
@@ -0,0 +1,31 @@
+﻿using ProyectoDatos.Dao;
+using System.Collections.Generic;
+using System;
+
+namespace ProyectoDatos
+{
+    class DBConeccion
+    {
+        public static void Main(String[] args)
+        {
+            DBConeccion conn = new DBConeccion();
+            conn.query();
+        }
+
+        private void query()
+        {
+            Ciudad c = new Ciudad();
+            IList<string> list = c.Query();
+            if (list == null)
+            {
+                Console.WriteLine("No data");
+                return;
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
